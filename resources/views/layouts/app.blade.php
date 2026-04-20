1f4211d11fa4cfe921dd3af49b8475610eae1964
Aghfar Maulana <maulana.1124020001@students.aknpacitan.ac.id>
2026-04-20 13:26:06 +0700

    tambah bundlig+user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,13 @@
         </li>
 
         <li class="nav-item">
+            <a class="nav-link" href="{{ route('bundling.index') }}">
+                <i class="fas fa-cash-register"></i>
+                <span>Bundling</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
             <a class="nav-link" href="{{ route('user.index') }}">
                 <i class="fas fa-cash-register"></i>
                 <span>User</span>
