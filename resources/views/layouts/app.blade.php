22701bc4843ecb1baed5e276b1875a1b4c856a4b
ferdian Syach <mp1581747@gmail.com>
2026-07-22 13:45:25 +0700

    baru

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -128,12 +128,12 @@
                 </a>
             </li>
 
-            <li class="nav-item {{ Request::is('rekomendasi*') ? 'active' : '' }}">
+            {{-- <li class="nav-item {{ Request::is('rekomendasi*') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('rekomendasi.index') }}">
                     <i class="fas fa-fw fa-users"></i>
                     <span>Rekomendasi Bundling</span>
                 </a>
-            </li>
+            </li> --}}
         @endif
             <hr class="sidebar-divider d-none d-md-block">
 
